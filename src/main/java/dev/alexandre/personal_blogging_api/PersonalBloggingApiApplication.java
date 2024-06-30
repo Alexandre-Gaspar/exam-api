b01db94aab880722b73e69d29a3ed578f687cf01
Alexandre Gaspar <alexandretiao16@gmail.com>
2024-06-30 21:37:55 +0100

    🎉 First commit

---
+++ src/main/java/dev/alexandre/personal_blogging_api/PersonalBloggingApiApplication.java
@@ -0,0 +1,13 @@
+package dev.alexandre.personal_blogging_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalBloggingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonalBloggingApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/alexandre/personal_blogging_api/PersonalBloggingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.alexandre.personal_blogging_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalBloggingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
